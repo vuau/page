abb58f312003aecd90aef5acbf7052ac6322eaea
Au <au@tinypulse.com>
2020-09-14 08:34:10 +0700

    update peer

--- src/contexts.js
+++ src/contexts.js
@@ -2,20 +2,19 @@ import Gun from 'gun/gun'
 import SEA from 'gun/sea'
 import 'gun/lib/not'
 import 'gun/lib/then'
-import 'gun/lib/webrtc'
 
 let gun
 
 if (process.env.NODE_ENV === 'production') {
-  gun = Gun(['https://pvaklb.ddns.net/gun'])
+  gun = Gun(['https://pensync.glitch.me/gun'])
 } else {
   gun = Gun(['http://localhost:8765/gun'])
 }
 
 const gunUser = gun.user()
 
+window.gun = gun
 if (process.env.NODE_ENV !== 'production') {
-  window.gun = gun
   window.gunUser = gunUser
 }
 
