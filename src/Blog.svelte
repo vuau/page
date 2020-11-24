138fbc51cb0e4d8ef68f518afd75e0c73db87a99
Au <au@tinypulse.com>
2020-11-24 10:15:33 +0700

    refactor

--- src/Blog.svelte
+++ src/Blog.svelte
@@ -2,75 +2,75 @@
   import { navigate } from 'svelte-routing'
   import { onMount } from 'svelte'
   import { gun } from './contexts.js'
-  import { getNode } from './stores.js'
+  import { getNode, getData as getData2 } from './stores.js'
   export let slug1
   export let slug2
   export let pub
 
-  let user, path, blog, pages, page, isLoading, isDoneAppend
-  onMount(() => {
+  const domainMap = {
+    'vuau.me': {
+      pub:
+        'J6ul10B2pvF1kr0ddHiEqtHbSsbzT06RtDQiJj90VhI.mQ5Ac2NgzzXGor_zgc0Hngl2-LVEN5frIhMju5r1HRc',
+      slug: 'blog'
+    },
+    'test.localhost': {
+      pub:
+        '_GUKWWJNelWVKV5XDF4REIXOeTi4z4I0U0RztCKYRkg._aaJK4Rpx9rz4c1vxL8ilmxS9lyT1GGeVG9_ToCmBE8',
+      slug: 'blog'
+    }
+  }
+
+  let user, path, blog, pages, page, isAppended, isLoading, isUseDomain
+
+  onMount(async () => {
     isLoading = true
-    user = gun.user(pub)
-    user
-      .get('slugs')
-      .get(slug1)
-      .on(v => {
-        path = v
-        if (path) {
-          getData()
-        }
-      })
+    if (!pub) {
+      isUseDomain = true
+      const domainInfo = getDomainInfo()
+      if (!domainInfo) {
+        isLoading = false
+        return
+      }
+      pub = domainInfo.pub
+      slug1 = domainInfo.slug
+    }
+    if (slug1) {
+      ;({ blog, pages } = await getData2({ slug: slug1, pub }))
+      injectHead(blog)
+    }
+    if (slug2) {
+      page = await getData2({ slug: slug2, pub })
+    }
+    isLoading = false
   })
+
+  function injectHead (blog) {
+    if (blog.headerTag && !isAppended) {
+      const doc = document
+        .createRange()
+        .createContextualFragment(blog.headerTag)
+      document.head.appendChild(doc)
+      isAppended = true
+    }
+  }
   function clickPage (href) {
     navigate(href)
     window.scrollTo(0, 0)
   }
   function getBlogURL () {
+    if (isUseDomain) {
+      return '/'
+    }
     return `/${slug1}/${pub}`
   }
   function getPageURL (slug) {
+    if (isUseDomain) {
+      return `/${slug}`
+    }
     return `/${slug1}/${slug}/${pub}`
   }
-  function getData () {
-    getNode(path, user).on(node => {
-      if (!node) return
-      if (node.type === 'folder') {
-        blog = node
-        if (blog.headerTag && !isDoneAppend) {
-          const doc = document
-            .createRange()
-            .createContextualFragment(blog.headerTag)
-          document.head.appendChild(doc)
-          isDoneAppend = true
-        }
-        pages = {}
-        getNode(path, user)
-          .get('children')
-          .map()
-          .on((node, id) => {
-            if (node && node.mode === 'public') {
-              pages[id] = node
-              if (node.slug === slug2) {
-                page = node
-                isLoading = false
-              }
-              // reactive
-              pages = pages
-              isLoading = false
-            }
-          })
-      } else {
-        page = node
-        if (page.headerTag && !isDoneAppend) {
-          const doc = document
-            .createRange()
-            .createContextualFragment(page.headerTag)
-          document.head.appendChild(doc)
-          isDoneAppend = true
-        }
-        isLoading = false
-      }
-    })
+  function getDomainInfo () {
+    return domainMap[location.hostname]
   }
 </script>
 
@@ -80,57 +80,56 @@
   </title>
 </svelte:head>
 
-<section class="mw8 pa3 center avenir">
-  {#if isLoading}
-    Loading...
-  {:else}
-    {#if blog && blog.title}
-      <h1 class="f2 f1-m f-headline-l pv2">
-        <a
-          on:click|preventDefault={() => clickPage(getBlogURL())}
-          class="link"
-          href={getBlogURL()}>
-          {blog.title}
-        </a>
-      </h1>
-    {:else if page && page.title}
-      <h2 class="f2 f2-m f-subheadline-l measure lh-title fw1 mt0 pb2">
-        {page.title}
-      </h2>
-    {/if}
-
-    {#if page}
-      {#if slug1 && slug2}
+{#if !isLoading}
+  {#if blog || page}
+    <section class="mw8 pa3 center avenir">
+      {#if blog && blog.title}
+        <h1 class="f2 f1-m f-headline-l pv2">
+          <a
+            on:click|preventDefault={() => clickPage(getBlogURL())}
+            class="link"
+            href={getBlogURL()}>
+            {blog.title}
+          </a>
+        </h1>
+      {:else if page && page.title}
         <h2 class="f2 f2-m f-subheadline-l measure lh-title fw1 mt0 pb2">
           {page.title}
         </h2>
       {/if}
-      <article>
-        {@html page.content}
-      </article>
-    {/if}
-    {#if !slug2 && blog}
-      {#if pages}
-        {#each Object.values(pages) as page}
-          <article class="bt b--black-10">
-            <a
-              on:click|preventDefault={() => clickPage(getPageURL(page.slug))}
-              class="db pv3 pv4-ns no-underline black dim"
-              href={getPageURL(page.slug)}>
-              <div class="flex flex-column flex-row-ns">
-                <h1 class="f3 fw1 baskerville mt0 lh-title">{page.title}</h1>
-              </div>
-            </a>
-          </article>
-        {/each}
+
+      {#if page}
+        {#if slug1 && slug2}
+          <h2 class="f2 f2-m f-subheadline-l measure lh-title fw1 mt0 pb2">
+            {page.title}
+          </h2>
+        {/if}
+        <article>
+          {@html page.content}
+        </article>
       {/if}
-    {/if}
-    {#if !blog && !page}Not found!{/if}
-  {/if}
-  <footer class="pv4 bt b--black-10 black-70">
-    <p class="f6 db b lh-solid">
-      © Powered by
-      <a class="link" href="https://app.denote.link">deNOTE</a>
-    </p>
-  </footer>
-</section>
+      {#if !slug2 && blog}
+        {#if pages}
+          {#each Object.values(pages) as page}
+            <article class="bt b--black-10">
+              <a
+                on:click|preventDefault={() => clickPage(getPageURL(page.slug))}
+                class="db pv3 pv4-ns no-underline black dim"
+                href={getPageURL(page.slug)}>
+                <div class="flex flex-column flex-row-ns">
+                  <h1 class="f3 fw1 baskerville mt0 lh-title">{page.title}</h1>
+                </div>
+              </a>
+            </article>
+          {/each}
+        {/if}
+      {/if}
+      <footer class="pv4 bt b--black-10 black-70">
+        <p class="f6 db b lh-solid">
+          © Powered by
+          <a class="link" href="https://app.denote.link">deNOTE</a>
+        </p>
+      </footer>
+    </section>
+  {:else}Not found.{/if}
+{/if}
--- src/stores.js
+++ src/stores.js
@@ -1,6 +1,6 @@
-import { gunUser } from './contexts'
+import { gun } from './contexts'
 
-export const getParentNode = (path, root = gunUser) => {
+export const getParentNode = (path, root) => {
   const parts = (path || '').split('_').filter(p => p !== '')
   let node = root.get('notes')
   parts.forEach(id => {
@@ -9,14 +9,40 @@ export const getParentNode = (path, root = gunUser) => {
   return node
 }
 
-export const getParentPath = (path) => {
+export const getParentPath = path => {
   let parts = (path || '').split('_') // eslint-disable-line
   const id = parts.pop()
   const parentPath = parts.join('_')
   return [id, parentPath]
 }
 
-export const getNode = (path, root = gunUser) => {
+export const getNode = (path, root) => {
   const [id, parentPath] = getParentPath(path)
   return getParentNode(parentPath, root).get(id)
+}
+
+// return { blog, pages } if getting a folder, return {...page} otherwise
+export async function getData ({ slug, pub }) {
+  const user = gun.user(pub)
+  const getPathFromSlugs = async slug => {
+    return await user
+      .get('slugs')
+      .get(slug)
+      .promOnce()
+  }
+  const path = await getPathFromSlugs(slug)
+  const node = getNode(path.data, user)
+  const nodeData = (await node.promOnce()).data
+
+  if (nodeData.type === 'folder') {
+    const children = node.get('children')
+    const childKeys = Object.keys((await children.promOnce()).data).filter(
+      key => key !== '_'
+    )
+    const pages = await Promise.all(
+      childKeys.map(async key => (await children.get(key).promOnce()).data)
+    )
+    return { blog: nodeData, pages }
+  }
+  return { ...nodeData }
 }
