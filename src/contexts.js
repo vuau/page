ab083eaae4d70f6516f837d9fcd18e2539b73aff
Au <au@tinypulse.com>
2021-03-22 13:49:24 +0700

    use community's gun server

--- src/contexts.js
+++ src/contexts.js
@@ -7,7 +7,11 @@ import '@gooddollar/gun/lib/promise'
 let gun
 
 if (process.env.NODE_ENV === 'production') {
-  gun = Gun(['https://pensync.glitch.me/gun', 'https://depen.xyz/gun'])
+  gun = Gun([
+    'https://pensync.glitch.me/gun',
+    'https://gun-us.herokuapp.com/gun',
+    'https://iris.cx/gun'
+  ])
 } else {
   gun = Gun(['http://localhost:8765/gun'])
 }
